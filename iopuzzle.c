6dee257bcef89de03385cdf0f52890d3a65a3426
Dmitry Goncharov <dgoncharov@users.sf.net>
2010-03-27 16:46:02 +0300

    added a comment which explains the behaviour of the program

--- iopuzzle.c
+++ iopuzzle.c
@@ -1,3 +1,11 @@
+/*
+ * Paragraph 7.9.3.7 of the C programming language standard ISO/IEC 9899:1999.
+ * "As initially opened, the standard error stream is not fully buffered;
+ * the standard input and standard output streams are fully buffered
+ * if and only if the stream can be determined
+ * not to refer to an interactive device."
+ */
+
 #include <stdio.h>
 #include <unistd.h>
 
